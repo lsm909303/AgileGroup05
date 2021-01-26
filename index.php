9f724aa086573ad5de5f0f0d76005e8818c11a50
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-01-26 14:06:58 +0000

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["LastName"]. " " . $row["FirstName"]. "<br>";
+    echo "id: " . $row["PersonID"]. " - Name: " . $row["LastName"]. " " . $row["FirstName"]. "<br>";
   }
 } else {
   echo "0 results";
