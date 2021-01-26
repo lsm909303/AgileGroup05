aae4c66df5cf845385a1d0f27956c653311e9e3d
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-01-26 14:05:12 +0000

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,25 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
+// Create connection
 $conn = new mysqli($server, $username, $password, $db);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["LastName"]. " " . $row["FirstName"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
 
 echo "Hello";
 
